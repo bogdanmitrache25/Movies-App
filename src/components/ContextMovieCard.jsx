ed227128e493de28b63ea77001e1508475e98e64
Bogdan Mitrache <bogdanfreshh@gmail.com>
2024-01-16 18:33:56 +0100

    first commit

--- src/components/ContextMovieCard.jsx
+++ src/components/ContextMovieCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { get } from "../data/httpClient";
 import MovieCard from "./MovieCard";
-import "../components/contextMovieCard.css";
+import "./contextMovieCard.css";
 
 const ContextMovieCard = () => {
   const [movies, setMovies] = useState([]);
